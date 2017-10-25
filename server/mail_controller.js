9776cacfbad0ba8e08887567fa5984016b48b65c
Mikel North <mikel.north@gmail.com>
2017-10-25 16:24:28 -0600

    styled email

--- server/mail_controller.js
+++ server/mail_controller.js
@@ -18,10 +18,27 @@ module.exports = {
         // setup email data with unicode symbols
         let mailOptions = {
             from: `"Karl North Media" <${process.env.user_email}>`, // sender address
-            to: process.env.admin_email, // list of receivers
+            to: process.env.user_email, // list of receivers
             subject: 'New Customer', // Subject line
             // text: 'testing one two on two', // plain text body
-            html: `<h1>${ req.body.firstName}  ${req.body.lastName}</h1> `// html body
+            html: `
+             <h3> Name:${ req.body.firstName}  ${req.body.lastName}</h3><br><br>
+             <span> Email:${req.body.email}</span><br><br>
+             <span> Phone:${req.body.phone}</span><br><br>
+             <span> Prefered contact method:${req.body.contact}</span><br><br>
+             ${ req.body.WeddingLocation ? `<span> Wedding Location:${req.body.bridalLocation}</span><br><br>`: ''}
+             ${ req.body.weddingDate ? `<span> Wedding Date:${req.body.weddingDate}</span><br><br>`: ''}
+             ${ req.body.bridalLocation ? `<span> Bridal Location:${req.body.bridalLocation}</span><br><br>`: ''}
+             ${ req.body.bridalDate ? `<span> Bridal Date:${req.body.bridalDate}</span><br><br>`: ''}
+             ${ req.body.receptionLocation ? `<span> reception Locatoin:${req.body.receptionLocation}</span><br><br>`: ''}
+             ${ req.body.receptionDate ? `<span> reception Date:${req.body.receptionDate}</span><br><br>`: ''}
+             ${ req.body.culture ? `<span> Wedding Culture:${req.body.culture}</span><br><br>`: ''}
+             ${ req.body.setting ? `<span> Setting:${req.body.setting}</span><br><br>`: ''}
+             ${ req.body.bridalLocation ? `<span> Bridal Location:${req.body.bridalLocation}</span><br><br>`: ''}
+             ${ req.body.audio ? `<span> Audio:${req.body.audio}</span><br><br>`: ''}                 
+             ${ req.body.message ? `<span> Aditonal Information:${req.body.message}</span><br><br>`: ''}
+             ${ req.body.bridalLocation ? `<span> Bridal Location:${req.body.bridalLocation}</span><br><br>`: ''}                     
+            `
         };
 
         // send mail with defined transport object
