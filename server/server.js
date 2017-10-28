fd14c6f6cd7a8177ab5dabef41e06a64f07381f7
Mikel North <mikel.north@gmail.com>
2017-10-27 18:20:05 -0600

    auth0 bug fix

--- server/server.js
+++ server/server.js
@@ -61,7 +61,7 @@ app.get('/auth/me', (req, res) => {
 
 app.get('/auth/logout', (req, res) => {
     req.logOut();
-    res.redirect(302, 'http://localhost:3000/')
+    res.redirect(302, process.env.REDIRECT)
 })
 
 app.get('/api/links/:category', controller.getLinks)
