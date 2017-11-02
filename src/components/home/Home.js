13898118fff2bb48a1c5b0a6953e23a5ce4b7c48
Mikel North <mikel.north@gmail.com>
2017-11-02 10:05:20 -0600

    safari test

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,7 +7,6 @@ import { withRouter } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 import Login from '../login/Login.js';
 import intro from '../../assets/intro.mp4';
-import test from '../../assets/test.mp4';
 import intro2 from '../../assets/intro2.webm';
 import karl from '../../assets/karl.jpg';
 import down from '../../assets/down.png';
@@ -70,8 +69,8 @@ class Home extends Component {
                 <div className='top'>
                     {/* <img className='homepic' src='http://warrenweddings.com/wp-content/uploads/2017/03/wedding-hire.jpg' /> */}
                     <MediaQuery query="(min-width: 750px)">
-                        <video className='loop' autoplay='autoplay' loop='loop' muted='true' playsinline fullscreen='false' poster="http://example.com/path/poster.jpg">>
-                            {<source src={test} type="video/mp4" />}
+                        <video className='loop' autoplay='autoplay' loop='loop' muted='true' playsinline fullscreen='false' poster="http://euonthemove.eu/wp-content/uploads/2017/05/no-video.jpg"> controls>
+                            {<source src={intro} type="video/mp4" />}
                             {<source src={intro2} type="video/webm" />}
                             Your browser does not support the video tag.
                      </video>
