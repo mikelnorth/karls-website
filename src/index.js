d1b44c6b41c1d1dc27a05bb0c42315e3a9f666be
Mikel North <mikel.north@gmail.com>
2018-02-01 23:03:29 -0700

    testing pwa

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { unregister } from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import { Provider } from 'react-redux';
 import store from './store'
 import {HashRouter} from 'react-router-dom'
@@ -13,4 +13,4 @@ ReactDOM.render(
         <App />
     </HashRouter></Provider>,
      document.getElementById('root'));
-unregister();
+     registerServiceWorker();
